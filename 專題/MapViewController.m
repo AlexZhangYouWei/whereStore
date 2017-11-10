13ed4205dbc6ccb2ae6ece96915ca7bc2b009206
user44 <user44@admin44.local>
2017-11-10 17:32:59 +0800

    no message

--- 專題/MapViewController.m
+++ 專題/MapViewController.m
@@ -99,28 +99,28 @@
 //    if(annotation == mapView.userLocation)
 //        return nil;
 //        myMKAnnotationView *resultView = (myMKAnnotationView*)[_theMapView dequeueReusableAnnotationViewWithIdentifier:@"Store"];
-//    
+//
 //    if(resultView==nil)
 //    {
 //        resultView = [[myMKAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"Store"];
 //    }
-//    
+//
 //    //resultView.image
 //    resultView.annotation = annotation;
-//    
-//    
+//
+//
 //    resultView.canShowCallout = YES;
-//    
+//
 //    UIButton *rightButton=[UIButton buttonWithType:UIButtonTypeDetailDisclosure];
-//    
+//
 //    [rightButton addTarget:self action:@selector(buttonPrssed:) forControlEvents:UIControlEventTouchUpInside];
-//    
+//
 //         resultView.rightCalloutAccessoryView=rightButton;
-//    
-//    
-//    
+//
+//
+//
 //    return resultView;
-//    
+//
 //}
 // 自行定義設定地圖標籤的函式
 
